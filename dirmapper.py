7628cf96c46061820a59e5b260188ca149e222e0
johnwez1 <j.f.weatherall@gmail.com>
2017-10-14 13:25:55 +1100

    dirmapper first commit

---
+++ dirmapper.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+place in read.me
+
+dirmapper v0.1
+
+development of prototype classes and methods
+
+the purpose of dirmapper is to read a codebase
+and present an easily decipherable map of 
+how files relate to each other and any
+external libraries
+
+the backend should contain relatively simple logic
+
+the frontend should be created using a markup language
+latex is easy but probably not practical as a dependancy
+
+lets try XML?
+
+
+this commented section to go into read.me after spyder git works
+"""
+
